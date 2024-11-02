c7b7504e8a423bd643c4f1a943d0b69b1703cb0f
Ankit Kumar <iamank.it1308@gmail.com>
2024-11-02 18:53:23 +0530

    operator

---
+++ Operators/Q1/Q1.cpp
@@ -0,0 +1,14 @@
+/*
+Q1. WAP to input two numbers and find its sum.
+*/
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b;
+  cout<<"Enter first number : ";
+  cin>>a;
+  cout<<"Enter second number : ";
+  cin>>b;
+  cout<<"Sum of two number : "<<a+b;
+  return 0;
+}
